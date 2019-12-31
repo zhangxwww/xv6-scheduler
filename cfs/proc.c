7fbe31d0265134dcf08df7f9bc338b590e3f0d3d
coffer <631133544@qq.com>
2020-01-01 05:16:35 +0800

    mlfq

--- cfs/proc.c
+++ cfs/proc.c
@@ -929,8 +929,9 @@ kill(int pid)
     if(p->pid == pid){
       p->killed = 1;
       // Wake process from sleep if necessary.
-      if(p->state == SLEEPING)
+      if(p->state == SLEEPING) {
         p->state = RUNNABLE;
+      }
         heappush(p);
       release(&ptable.lock);
       return 0;
